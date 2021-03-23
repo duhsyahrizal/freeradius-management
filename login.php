3b1aef6d13404e8bade9132ed6f9bab4a6176af9
Muhammad Abduh Syahrizal <cigs.abduh@gmail.com>
2021-03-23 10:45:29 +0700

    update icon logo

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Login Page</title>
+  <link rel="shortcut icon" href="customer/img/logo.png">
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php
